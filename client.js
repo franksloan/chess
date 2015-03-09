32a7b5b985057d7e53ffe8ce2bbe6e84a453da67
Frank Sloan <francis.m.sloan@gmail.com>
2015-03-09 18:03:19 +0000

    pre refactor

---
+++ client.js
@@ -0,0 +1,146 @@
+var socket = io.connect('http://localhost:3000');
+socket.on('connect', function(data){
+  if(match.player1.name === null || match.player2.name === null) {
+    nickname = prompt("What's your name?");
+    if(match.player1.name === null){
+      //with whose go
+      socket.emit('join', nickname, true);
+    } else {
+      socket.emit('join', nickname, false);
+    }
+  } else {
+    alert('There are already two players!');
+  }  
+});
+socket.on('join', function(name){
+  if(match.player1.name === null) {
+    match.player1.name = name;
+  } else {
+    if(match.player2.name === null) {
+      match.player2.name = name;
+    }
+  }
+});
+// socket.on('setName', function(nickname){
+//   match.player1.name 
+// });
+document.addEventListener('DOMContentLoaded', function(){
+  var chatInput = document.getElementById("chat-input");
+  chatInput.addEventListener('keypress', function(e){
+    if(e.keyCode === 13) {
+      var message = chatInput.value;
+      // var message = document.createTextNode(chatInput.value);
+      // console.log(message);
+      socket.emit('messages', message);
+      chatInput.value = ''; 
+    }
+  });
+  socket.on('messages', function(data){
+    var messages = document.getElementById("message-list");
+    var messageItem = document.createElement("li");
+    var mes = document.createTextNode(data);
+    messageItem.appendChild(mes);
+    messages.appendChild(messageItem);
+  });
+
+  var button = document.querySelector("button");
+  var squares = document.querySelectorAll("div .square");
+  //message list in chat
+  var messages = document.getElementById("message-list");
+  var removeClass = function(array, i){
+    array[i].className = array[i].className.substring(0,12);
+  }
+  var doClass = function(name, array, fnc) {
+    for(var i = 0; i < array.length; i++) {
+      if(array[i].className.search(name) >= 0) {
+        fnc(array, i);
+      }
+    }
+  }
+  var fromSet = false;
+  var toSet = false;
+  for(var i = 0; i < squares.length; i++) {
+    console.log('enough');
+    squares[i].addEventListener('click', function(){
+      if(this.className.search('from') >= 0) {
+        //fromSet to false
+        socket.emit('highlight', false, null, this.id);
+      } else {
+        if(!fromSet) {
+          if(this.className.search('to') == -1){
+            //fromSet to true
+            socket.emit('highlight', true, null, this.id);
+          } else {
+            //toSet to false
+            socket.emit('highlight', null, false, this.id);
+          }
+        } else {
+          if(this.className.search('to') >= 0) {
+            //toSet to false
+            socket.emit('highlight', null, false, this.id);
+          } else {
+            if(!toSet) {
+              //toSet to true
+              socket.emit('highlight', null, true, this.id);
+            }
+          }
+          
+        }
+      }
+    });
+  }
+  //receive square pressing
+  socket.on('highlight', function(fromS, toS, id){
+    console.log(fromSet + ' or ' + toSet + ' at ' + id);
+    if (fromS !== null){
+      fromSet = fromS;
+      if(fromSet){
+        document.getElementById(id).className += ' from';
+      } else {
+        doClass('from', squares, removeClass);
+      }
+    }
+    if (toS !== null){
+      toSet = toS;
+      if(toSet){
+        document.getElementById(id).className += ' to';
+      } else {
+        doClass('to', squares, removeClass);
+      }
+    }
+
+  });
+  button.addEventListener('click', function(){
+    if(fromSet && toSet){
+      var fromX = parseInt(document.querySelector('.from').id[1]);
+      var fromY = parseInt(document.querySelector('.from').id[3]);
+      var toX = parseInt(document.querySelector('.to').id[1]);
+      var toY = parseInt(document.querySelector('.to').id[3]);
+      var from = [fromX, fromY];
+      var to = [toX, toY];
+      var player = match.playerTurn['name'];
+      socket.emit('move', from, to, player);
+    } else {
+      console.log('Need a start (blue) and end (green) destination')
+    } 
+  });
+  socket.on('move', function(from, to){
+    match.turn(from, to);
+      if(match.complete){
+        var piece = document.querySelector('.from').innerHTML;
+        document.querySelector('.from').innerHTML = null;
+        doClass('from', squares, removeClass);
+        fromSet = false;
+        document.querySelector('.to').innerHTML = piece;
+        doClass('to', squares, removeClass);
+        toSet = false;
+      } else {
+        //
+        var messageItem = document.createElement("li");
+        var textnode = document.createTextNode("Chessboard: " + match.message);
+        messageItem.appendChild(textnode);
+        messages.appendChild(messageItem);
+        //
+      }
+  });
+});
--- server.js
+++ server.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
-
-// var path = require('path');
-
+var chess = require('./chess');
 
 app.use(express.static(__dirname));
 app.get('/', function(req, res) {
@@ -13,16 +10,52 @@ app.get('/', function(req, res) {
   res.sendFile('/index.html');
 });
 
+var board = ["----------",
+            "-CHBQKBHC-",
+            "-PPPPPPPP-",
+            "-        -",
+            "-        -",
+            "-        -",
+            "-        -",
+            "-PPPPPPPP-",
+            "-CHBQKBHC-",
+            "----------"];
+
 io.on('connection', function(client) {
 	console.log('Client connected');
-
-	
+	var match = new chess.Match(board, {"-": chess.Edge, "P": chess.Pawn, "H": chess.Knight, "C": chess.Castle, 
+								"B": chess.Bishop, "Q": chess.Queen, "K": chess.King});
+	console.log(match.toString());
+	client.on('join', function(name, turn){
+		client.nickname = name;
+		client.broadcast.emit('join', name);
+		client.emit('join', name);
+		client.turn = turn;
+	});
+	//exchange messages
 	client.on('messages', function(data){
+		var nickname = client.nickname;
 		console.log(data);
-		var world = data;
-		client.broadcast.emit('messages', world);
+		var message = data;
+		client.broadcast.emit('messages', nickname + ': ' + message);
+		client.emit('messages', nickname + ': ' + message);
 	})
-
+	//pick squares
+	client.on('highlight', function(fromSet, toSet, id){
+		console.log(fromSet + ' or ' + toSet + ' at ' + id);
+		client.broadcast.emit('highlight', fromSet, toSet, id);
+		client.emit('highlight', fromSet, toSet, id);
+	});
+	//make the move
+	client.on('move', function(from, to, playerName){
+		if(playerName === client.nickname){
+			console.log('from: ' + from + ' and to: ' + to);
+			client.broadcast.emit('move', from, to);
+			client.emit('move', from, to);
+			
+		}
+		
+	});
 	
 });
 
